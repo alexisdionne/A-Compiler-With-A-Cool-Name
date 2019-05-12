34be269b60eede2004e2f573661393a4cc6388a2
Alexis Dionne <alexis.dionne98@gmail.com>
2019-05-12 00:50:43 -0400

    It is working with if statements if the result is true, but for some reason it doesn't like false evaluations. To be continued..

--- CodeGen.py
+++ CodeGen.py
@@ -13,6 +13,7 @@ class CodeGen:
   code = []
   arrPos = 0
   jump = {}
+  jumpCount = 0
   staticVar = {}
   staticCount = 0
   
@@ -29,26 +30,33 @@ class CodeGen:
     s.code.append('00')
     s.arrPos += 1
     s.backpatch()
+    for x in range(s.arrPos, 256):
+      s.code.append('00')
     print()
     s.printCode()
     
   def analyze(s, node):
     # move through AST and scope tree to generate code
     # call the proper method for each node that comes up
+    # print()
+    # print("NODE: ",node.name,"pos: ", s.arrPos)
     if node.name is 'Block':
       s.blockGen(node)
     elif node.name is 'VarDecl':
       s.varDeclGen(node)
     elif node.name is 'Assignment':
       s.assignmentGen(node)
-    # elif node.name is 'If' or node.name is 'While':
-      # s.booleanGen(node.children[0])
-      # s.blockGen(node.children[1])
+    elif node.name is 'If':
+      s.ifGen(node.children[0])
+      s.blockGen(node.children[1])
+    # elif node.name is 'While':
+      
     elif node.name is 'Print':
       s.printGen(node.children[0])
     # else
       # nothing happens here because any other nodes dont need special care
-      
+    # s.printCode()
+    
     # dig deeper if there are more kiddos
     if len(node.children) is not 0:
       for i in range(len(node.children)):
@@ -56,6 +64,8 @@ class CodeGen:
       if node.name == 'Block':
         # return to the parent of this scope
         s.scopeTree.returnToParent()
+      elif node.name == 'If':
+        s.jump[s.jumpCount-1] = str(s.arrPos - s.jump[s.jumpCount-1])
         
   def blockGen(s, node):
     # move up a scope level to grab correct data location
@@ -77,10 +87,18 @@ class CodeGen:
   def assignmentGen(s, node):
     # load the accumulator with the number to assign and 
     # store that in the id's memory location
-    s.code.append('A9')
-    s.arrPos += 1
-    s.code.append('0'+node.children[1].name)
-    s.arrPos += 1
+    if node.children[1].name in s.id:
+      s.code.append('AD')
+      s.arrPos += 1
+      s.code.append('T'+s.findStatic(node.children[1]))
+      s.arrPos += 1
+      s.code.append('XX')
+      s.arrPos += 1
+    elif node.children[1].name in s.intList:
+      s.code.append('A9')
+      s.arrPos += 1
+      s.code.append('0'+node.children[1].name)
+      s.arrPos += 1
     s.code.append('8D')
     s.arrPos += 1
     s.code.append("T"+s.findStatic(node.children[0]))
@@ -122,22 +140,49 @@ class CodeGen:
       s.code.append('FF')
       s.arrPos += 1
     
+  def ifGen(s, node):
+    # check if the two values are equivelent
+    # and branch depending
+    
+    s.code.append('AE')
+    s.arrPos += 1
+    s.code.append('T' + s.findStatic(node.children[0]))
+    s.arrPos += 1
+    s.code.append('XX')
+    s.arrPos += 1
+    s.code.append('EC')
+    s.arrPos += 1
+    s.code.append('T' + s.findStatic(node.children[1]))
+    s.arrPos += 1
+    s.code.append('XX')
+    s.arrPos += 1
+    s.code.append('D0')
+    s.arrPos += 1
+    s.code.append(s.newJump())
+    s.arrPos += 1
+  
   def backpatch(s):
     # go back through the code generated and replace any static variables
     for i in s.staticVar:
       address = hex(i + s.arrPos)[2:]
       if len(address) < 2:
         address = "0"+address
-      print('address of ',i,': ',address)
+      #print('address of ',i,': ',address)
       s.staticVar[i][1] = address
     for x in range(len(s.code)):
       if 'T' in s.code[x]:
         # there is a T which identifies a variable
         # the second letter in the string identifies the unique key
         key = s.code[x][1]
-        print("replacing T",key," with ",s.staticVar[int(key)][1])
+        #print("replacing T",key," with ",s.staticVar[int(key)][1])
         s.code[x] = s.staticVar[int(key)][1]
         s.code[x+1] = '00'
+      elif 'J' in s.code[x]:
+        key = s.code[x][1]
+        if len(s.jump[int(key)][0]) < 2:
+          s.code[x] = '0' + s.jump[int(key)][0]
+        else:
+          s.code[x] = s.jump[int(key)][0]
         
   
   # Helper methods
@@ -165,9 +210,9 @@ class CodeGen:
     # check for the id in the static variables table and return temp number
     temp = "ERROR"
     for x in s.staticVar:
-      print('x is',s.staticVar[x])
+      #print('x is',s.staticVar[x])
       if s.staticVar[x][0] == node.name:
-        print("found ",node.name)
+        #print("found ",node.name)
         temp = str(x)
     return temp
     
@@ -180,3 +225,10 @@ class CodeGen:
     else:
       type = "ERROR"
     return type
+    
+  def newJump(s):
+    # create a new jump distance in jump table
+    s.jump[s.jumpCount] = s.arrPos
+    temp = "J" + str(s.jumpCount)
+    s.jumpCount += 1
+    return temp
